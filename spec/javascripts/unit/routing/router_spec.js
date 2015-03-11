944ce62c667a4eb18569b58810450402b6d7b4ad
Gharbi Mohammed <simo163@hotmail.co.uk>
2015-03-11 23:38:48 +0100

    create a contacts routes to ember

--- spec/javascripts/unit/routing/router_spec.js
+++ spec/javascripts/unit/routing/router_spec.js
@@ -11,3 +11,11 @@ test('root route',function(){
         equal(current_route, 'index', 'Expected index got: ' + current_route);
 	}); 
 });
+
+test('contacts route',function(){ 
+	visit('/contacts');
+	andThen(function () {
+		var current_route = AddressBook.__container__.lookup('controller:application').currentRouteName;
+        equal(current_route, 'contacts', 'Expected contacts got: ' + current_route);
+	}); 
+});
