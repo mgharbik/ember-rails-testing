286247de99358d329b2b97b8d2b44a46ba942b05
Gharbi Mohammed <simo163@hotmail.co.uk>
2015-03-15 16:43:44 +0100

    refactorFixtureAdapter

--- spec/javascripts/integration/contacts_integrations_spec.js
+++ spec/javascripts/integration/contacts_integrations_spec.js
@@ -1,18 +1,5 @@
 module('Contacts integration', { 
-	setup: function () {
-		AddressBook.ApplicationAdapter = DS.FixtureAdapter;
-		AddressBook.Contact.FIXTURES = [
-			{
-				id: 1,
-				first_name: 'Dave',
-				last_name: 'Crack'
-			}, {
-				id: 2,
-				first_name: 'Dustin',
-				last_name: 'Hoffman'
-			}
-		]
-	}, 
+	setup: function () { }, 
 	teardown: function () {
     	AddressBook.reset();
 	}
--- spec/javascripts/spec_helper.js
+++ spec/javascripts/spec_helper.js
@@ -8,3 +8,16 @@ d.write('<div id="ember-testing-container"> <div id="ember-testing"></div></div>
 AddressBook.rootElement = "#ember-testing";
 AddressBook.setupForTesting();
 AddressBook.injectTestHelpers();
+
+AddressBook.ApplicationAdapter = DS.FixtureAdapter;
+AddressBook.Contact.FIXTURES = [
+	{
+		id: 1,
+		first_name: 'Dave',
+		last_name: 'Crack'
+	}, {
+		id: 2,
+		first_name: 'Dustin',
+		last_name: 'Hoffman'
+	}
+]
